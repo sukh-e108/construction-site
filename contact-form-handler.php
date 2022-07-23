e70239ace8a6368e95b1c85369ab36c3fceaae7e
sukh-e108 <83979237+sukh-e108@users.noreply.github.com>
2022-07-23 17:08:07 +0530

    Add files via upload
    this updated version

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,32 +1,33 @@
 <?php
 
-$name = $_Post['name'];
+$name = $_POST['name'];
 
-$visitor_email = $_Post['email'];
+$visitor_email = $_POST['email'];
 
-$phone_number = $_Post['pnumber'];
+$phone_number = $_POST['pnumber'];
 
-$message = $_Post['message'];
+$message = $_POST['message'];
 
 $email_from = 'BJKManagement.com';
 
 $email_subject = "New Form Submission";
 
-$email_body = "User Name: $name.\n".
-                    "User Email: $email.\n".
-                         "User Message: $message.\n".
-                              "User Phone No: $pnumber.\n";
+$email_body = "User Name: $name. \n".
+                    "User Email: $visitor_email. \n".                         
+                              "User Phone No: $phone_number. \n".
+                                   "User Message: $message. \n";
 
                 
 
-$to = "therisinglord000@gmail.com";
+$to = "bjkmanagementltd@hotmail.com";
 
-$headers = "From $email_from \r\n";
+$headers = "From: $email_from \r\n";
 
 $headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
-header("Location:contact-us");
+header("Location:contact-us.html");
+
 ?>
 
